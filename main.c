a4689d0d506531faf6b50e748b80cbcbce872378
leomuso <Pinosenx@gmail.com>
2024-10-15 17:46:08 +0200

    exercise 4.5

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-        int numeroProdotti, i = 1;
+        int numeroProdotti, i;
         float costo, totale = 0;
         printf("Quanti prodotti hai acquistato? ");
         scanf("%d", &numeroProdotti);
-
-        while (i <= numeroProdotti) {
+        for (i = 1; i <= numeroProdotti; i++) {
+            printf("Inserisci il costo del prodotto %d: ", i);
+            scanf("%f", &costo);
+            totale += costo;
+        }
+       /* while (i <= numeroProdotti) {
             printf("Inserisci il costo del prodotto %d: ", i);
             scanf("%f", &costo);
             totale += costo;
             i++;
-        }
+        }*/
         printf("Il totale da pagare e': %.2f\n", totale);
 
         return 0;
